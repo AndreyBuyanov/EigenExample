109eba2d2f5050edf239af9075b04f13d9cbb4cb
AndreyBuyanov <andreew.12.87@gmail.com>
2021-05-23 19:18:59 +0700

    Initial commit

---
+++ Example.01/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+#include <Eigen/Dense>
+
+int main (int argc, char *argv[]){
+    Eigen::MatrixXf m(2, 2);
+    m << 1, 2, 3, 4;
+    Eigen::VectorXf v(2);
+    v << -1, 1;
+    auto r = m * v;
+    std::cout << r(0) << " " << r(1) << std::endl;
+    return 0;
+}
